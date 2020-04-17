3c319079967f682cce49d1187c74602e9f30323c
Jose <pjose14.jp38@yahoo.com>
2020-04-17 15:29:56 -0500

    addItema and removeItem are functional. MVP completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import { ProductContext } from './components/contexts/ProductContext';
+import { CartContext } from './components/contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -17,22 +20,30 @@ function App() {
 	};
 
 	const removeItem = id => {
-		
+		setCart(cart.filter(item => item.id !== id));
 	}
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart, removeItem }}>
+				<div className="App">
+					<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
+	
 	);
 }
 
